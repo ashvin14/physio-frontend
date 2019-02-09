ac385e3c2b442e830576b2507f9764f946fa65f3
ashvin14 <yashkhrnr@gmail.com>
2019-02-09 11:39:46 +0530

    generated a build folder

---
+++ build/precache-manifest.1eda3580c2c86005cd7c7c900ccd0cb8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "4bad963cf3d8c79a4fdb",
+    "url": "/static/css/main.7348bcc6.chunk.css"
+  },
+  {
+    "revision": "4bad963cf3d8c79a4fdb",
+    "url": "/static/js/main.4bad963c.chunk.js"
+  },
+  {
+    "revision": "2cad568af044f3124bdf",
+    "url": "/static/js/Signin.2cad568a.chunk.js"
+  },
+  {
+    "revision": "d186f7c6d8dce2e1ee66",
+    "url": "/static/css/2.f3d07740.chunk.css"
+  },
+  {
+    "revision": "d186f7c6d8dce2e1ee66",
+    "url": "/static/js/2.d186f7c6.chunk.js"
+  },
+  {
+    "revision": "11eceb5b5b686c3bed0b",
+    "url": "/static/js/runtime~main.11eceb5b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/static/media/notification.c0d3c94c.eot"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "0e3b433ae03a5ca68a2e76d0b377151c",
+    "url": "/index.html"
+  }
+];
