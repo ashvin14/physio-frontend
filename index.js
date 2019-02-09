67fa1bf02bb2bc5e0b18bda6d4c1f4a0adbdc6a7
ashvin14 <yashkhrnr@gmail.com>
2019-02-09 12:18:40 +0530

    removed errors

--- index.js
+++ index.js
@@ -1,15 +1,4 @@
 const express = require("express");
-const path = require("path");
-const app = express();
-
-app.use(express.static(path.join(__dirname, "build")));
-
-app.get("/*", function(req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
-
-app.listen(9000);
-const express = require("express");
 const favicon = require("express-favicon");
 const path = require("path");
 const port = process.env.PORT || 8080;
